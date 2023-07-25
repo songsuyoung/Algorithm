3162f50c859dbc899b0f4ea432eb9ede7ab1baec
suyoung <81684148+songsuyoung@users.noreply.github.com>
2023-07-25 14:55:03 +0900

    백준 2606 바이러스 bfs
    너비 우선 탐색 방법으로 푼 바이러스로, 배열의 크기가 중요시 된다.

---
+++ 2606.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int bfs(int N,bool *isVisited,bool computers[][102]){ //bfs 너비우선탐색 queue dfs 깊이우선탐색 stack
+
+    queue<int> q;
+    
+    q.push(1);
+    int cnt=0;
+    
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        
+        if(isVisited[node])
+            continue;
+        
+        isVisited[node]=true;
+        cnt++;
+        
+        for(int i=1;i<=N;i++){
+            if(!isVisited[i]&&computers[node][i]){
+                q.push(i);
+            }
+        }
+    }
+    
+    return cnt-1; //자기 자신을 제외한다.
+}
+
+int main()
+{
+    int n,e;
+    
+    cin>>n>>e;
+    
+    bool computers[150][150];
+    
+    for(int i=0;i<e;i++){
+        int n1,n2;
+        cin>>n1>>n2;
+        computers[n1][n2]=true;
+        computers[n2][n1]=true;
+    }
+    
+    bool isVisited[150]={false,};
+    
+    cout<<bfs(n,isVisited,computers);
+    
+    return 0;
+}
