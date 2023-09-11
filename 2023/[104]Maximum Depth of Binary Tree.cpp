3ab9498434d68f0dd33df0b09ed9e17cde29444c
suyoung <81684148+songsuyoung@users.noreply.github.com>
2023-09-11 09:45:29 +0900

    [LeetCode] 104. Maximum Depth of Binary Tree
    - 이동할 때마다 1씩 더해준다.
    - non leaf노드의 경우 왼쪽 오른쪽 깊이를 확인하여 max값을 반납한다.
    - 루트에 도달할 때까지 반복한다.

---
+++ 2023/[104]Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+
+    int maxDepth(TreeNode* root) {
+        if(root==nullptr){
+            return 0;
+        }
+
+        int left=1+maxDepth(root->left);
+        int right=1+maxDepth(root->right);
+
+        return max(left,right); 
+    }
+};
