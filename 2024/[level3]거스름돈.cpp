5def138ed1607a1a244cdb8743ffbea677706ebd
suyoung <81684148+songsuyoung@users.noreply.github.com>
2024-07-15 10:39:48 +0900

    [level3] 거스름돈

---
+++ 2024/[level3]거스름돈.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(int n, vector<int> money) {
+
+	vector<int> dp(n+1,0);
+
+	dp[0] = 1;
+
+	for (int i = 0; i < money.size(); i++)
+	{
+		for (int j = money[i]; j <= n; j++)
+		{
+			dp[j] = dp[j] + dp[j - money[i]];
+		}
+	}
+    return dp[n];
+}
