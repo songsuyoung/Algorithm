9badafe1a29ebb77b4e9a5b9a8c783441171dce8
suyoung <81684148+songsuyoung@users.noreply.github.com>
2025-03-20 12:49:00 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout << "|\\_/|\n";
+	cout << "|q p|   /}\n";
+	cout << "( 0 )\"\"\"\\\n";
+	cout << "|\"^\"`    |\n";
+	cout << "||_/=\\\\__|\n";
+
+}
+
+
+/*
+
+*/
