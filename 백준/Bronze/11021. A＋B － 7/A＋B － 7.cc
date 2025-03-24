abaed71b7895316148068831ccc52e1ccd896bba
suyoung <81684148+songsuyoung@users.noreply.github.com>
2025-03-24 09:39:21 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		cout << "Case #"<<i+1<<": "<<a + b << '\n';
+	}
+	
+}
