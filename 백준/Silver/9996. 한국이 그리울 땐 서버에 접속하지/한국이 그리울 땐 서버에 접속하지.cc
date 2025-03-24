82f7fc2a0ecc948455affa6f7344cddd43006548
suyoung <81684148+songsuyoung@users.noreply.github.com>
2025-03-24 12:03:42 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -1,72 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<string> split(string str, string term)
-{
-	int pos = 0;
-
-	vector<string> v;
-	while ((pos = str.find(term)) != string::npos)
-	{
-		string tmp = str.substr(0, pos);
-		v.push_back(tmp);
-		str.erase(0, pos + term.length());
-	}
-	v.push_back(str);
-
-	return v;
-}
-
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
+	cout.tie(0);
 
 	int N;
 	cin >> N;
 
 	string pattern;
-
 	cin >> pattern;
 
-	vector<string> plist = split(pattern,"*");
+	int starIdx = pattern.find('*');
 
-	for (int i = 0; i<N; i++)
+	string prefix = pattern.substr(0, starIdx); // 처음까지
+	string suffix = pattern.substr(starIdx + 1); //끝까지
+
+	for (int i = 0; i < N; i++)
 	{
 		string input;
-
 		cin >> input;
 
-		string answer = "DA";
-		
-		int idx = input.find(plist[0]);
-
-		if (idx != 0)
+		if (input.size() < (prefix.size() + suffix.size()))
 		{
-			answer = "NE";
+			//작으면, NE
+			cout << "NE\n";
+			continue;
+		}
+
+		//앞, 뒤로 자른다.
+
+		string pre = input.substr(0, prefix.size()); //0부터 prefix 사이즈만큼
+		string suf = input.substr(input.size() - suffix.size()); //suffix 사이즈만큼
+
+		if (pre == prefix && suffix == suf)
+		{
+			cout << "DA\n";
 		}
 		else
 		{
-			input.erase(0, idx + plist[0].length());
+			cout << "NE\n";
 		}
-
-		if (input.length() < plist[1].length())
-		{
-			answer = "NE";
-		}
-
-		while ((idx = input.find(plist[1])) != string::npos)
-		{
-			if(answer == "NE") break;
-			//끝
-			input.erase(0, idx + plist[1].length());
-		}
-		
-		if (idx + plist[1].length() < input.length())
-		{
-			answer = "NE";
-		}
-
-		cout << answer << '\n';
 	}
 }
+
