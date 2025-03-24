3bf61f3ce70c1c14e3374ce8054d881fa00c9d2e
suyoung <81684148+songsuyoung@users.noreply.github.com>
2025-03-24 09:26:45 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int A, B;
+		cin >> A >> B;
+
+		cout << A + B << '\n';
+	}
+}
