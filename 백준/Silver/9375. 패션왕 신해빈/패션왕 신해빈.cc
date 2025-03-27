d67e42641953cc6fa654a4a00af5dd4b1ad6e862
suyoung <81684148+songsuyoung@users.noreply.github.com>
2025-03-27 10:50:04 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -1,50 +1,41 @@
 #include <bits/stdc++.h>
-#include <unordered_map>
 using namespace std;
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
+	cout.tie(0);
 
-	int N;
-	cin >> N;
+	int TC;
 
-	for (int i = 0; i < N; i++)
+	cin >> TC;
+
+	for (int i = 0; i < TC; i++)
 	{
-		vector<string> v;
-		unordered_map<string, int> cnt;
+		int N;
+		cin >> N;
 
-		int M;
-		cin >> M;
+		unordered_map<string, int> garbs;
 
-		for (int j = 0; j < M; j++)
+		for (int j = 0; j < N; j++)
 		{
 			string name, type;
+
 			cin >> name >> type;
-			
-			if (cnt.find(type) == cnt.end())
-			{
-				//없을 때 추가
-				v.push_back(type);
-			}
-			cnt[type]++; //같은 이름을 가진 의상은 없기 때문에 카운트만 센다.	
+
+			garbs[type]++;
 		}
 
-		int answer = 1;
-
-		for (int i = 0; i < v.size(); i++)
+		int total = 1;
+		for (const auto& garb : garbs)
 		{
-			//옷을 안입는 경우의 수를 포함
-			answer *= (cnt[v[i]] + 1);
+			total *= (garb.second + 1);
 		}
+		total -= 1;
 
-		//전부 안입었을때 1을 빼준다.
-		cout << answer - 1 << '\n';
+		cout << total << '\n';
 	}
+
+
 }
-
-
-/*
-
-*/
