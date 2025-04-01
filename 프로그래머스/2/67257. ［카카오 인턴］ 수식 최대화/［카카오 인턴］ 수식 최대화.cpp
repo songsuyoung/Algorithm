e444155836ec0c744fb7282f56c8ec5508a39b4c
suyoung <81684148+songsuyoung@users.noreply.github.com>
2025-04-01 14:11:44 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.03 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.cpp
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.cpp
@@ -1,122 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void split(string expression, vector<char> &expOp, vector<long long> &num)
-{
-    string str = "";
+long long solution(string expression) {
+    long long answer = LLONG_MIN;
+    
+    char op[6][3] = {{'*','+','-'},{'*','-','+'},{'+','*','-'},{'+','-','*'},{'-','+','*'},{'-','*','+'}};
+    
+    vector<long long> num;
+    vector<char> expOP;
+    
+    string numToInt="";
     for(int i=0; i<expression.size(); i++)
     {
-        if(expression[i] == '*'||expression[i] == '-' || expression[i]=='+')
+        if(expression[i]>='0' && expression[i]<='9')
         {
-            num.push_back(stoi(str));
-            expOp.push_back(expression[i]);
-            str = "";
+            numToInt+=expression[i];
         }
         else
         {
-            str += expression[i];
+            if(numToInt.empty() == false)
+            {
+                num.push_back(stoll(numToInt));
+            }
+            numToInt ="";
+            expOP.push_back(expression[i]);
         }
     }
-     num.push_back(stoi(str));
-}
-
-long long solution(string expression) {
-    long long answer = 0;
-    
-    //만들 수 있는 연산자 => 미리 정의
-    char op[6][3] = {{'*','+','-'},{'*','-','+'},{'+','*','-'},{'+','-','*'}, {'-','+','*'},{'-','*','+'}};
-    
-    //split한다.
-    vector<long long> numBuffer;
-    vector<char> expOpBuffer;
-    
-    split(expression, expOpBuffer, numBuffer);
+    if(numToInt.empty() == false)
+    {
+        num.push_back(stoll(numToInt));
+    }
     
     for(int i=0; i<6; i++)
     {
-        vector<long long> num = numBuffer;
-        vector<char> expOp = expOpBuffer;
-        for(int k=0; k<3; k++)
-        {
-            while(find(expOp.begin(), expOp.end(), op[i][k]) != expOp.end())
+        vector<long long> numBuf = num;
+        vector<char> expOPBuf = expOP;
+        for(int j=0; j<3; j++)
+        {   
+            while(find(expOPBuf.begin(), expOPBuf.end(), op[i][j])!=expOPBuf.end())
             {
-                int j = find(expOp.begin(), expOp.end(), op[i][k]) - expOp.begin(); //실제 인덱스를 가져올 수 있음
+               int idx = find(expOPBuf.begin(), expOPBuf.end(), op[i][j]) - expOPBuf.begin();
                 
-                //연산 j - j+1 연산을 수행
-                long long res=0;
-                switch(op[i][k])
+                long long num1 = numBuf[idx];
+                long long num2 = numBuf[idx+1];
+                
+                long long sum = 0;
+                switch(op[i][j])
                 {
-                    case '+':
-                        res = num[j] + num[j+1];
-                        break;
                     case '*':
-                        res = num[j] * num[j+1];
+                        sum = num1 * num2;
                         break;
                     case '-':
-                        res = num[j] - num[j+1];
+                        sum = num1 - num2;
+                        break;
+                    case '+':
+                        sum = num1 + num2;
                         break;
                 }
-
-                        //j와 j+1 제거하고 j위치에 삽입
-                int num1 = num[j], num2 = num[j+1];
                 
-                num.erase(num.begin()+j);
-                num.erase(num.begin()+j);
-                expOp.erase(expOp.begin()+j);
-                //remove(num.begin(), num.end(), num1);
-                //예를 들어) 200 + 100 => 300 이어야하는데...
-                // 100 + 100 + 100 +는 0,1번 idx
-                // 200 + 100 =>실제로 idx = 0번이지만.... 1번으로 인식해 1-2랑 계산을 해버림..
-                num.insert(num.begin()+j, res);
-                //한번만 연산하구나,.?
+                numBuf.erase(numBuf.begin()+idx);
+                numBuf.erase(numBuf.begin()+idx);
+                
+                expOPBuf.erase(expOPBuf.begin()+idx);
+                
+                numBuf.insert(numBuf.begin() + idx, sum);
             }
         }
-        answer = max(answer, abs(num[0]));
+        answer= max(answer, abs(numBuf[0]));
     }
-    
     return answer;
 }
 
 /*
-IT 라이언
-해커톤 대회에 참여한 자는 숫자들과 3가지 연산으로 + - * 만으로 이루어진 연산 수식이 전달
-전달받은 수식에 포함된 연산자의 우선순위를 자유롭게 재정의하여 만들 수 있는 큰 숫자를 제출
-예)
 
-100-200*300-500+20
-+ > - > *
-=> 절대값 사용.
-
-100-(200*300)-500+20
-=> 100-60000-500+20
-=> 100-60000-(500+20)
-=> 100-60000-520
-=> ..
-
-숫자를 저장
-100 200 300 500 20
--*-+
-
-우선 순위를 정한다. * + -/ * - + / + - * ... 6가지
-
-split 한다.
-100 200 300 500 20
-   +   *   -  +
-
-연산자 배열에서 우선순위 1위를 찾는다. 
-* 먼저 : * idx = 1번 num[idx] * num[idx+1]
-* num[idx]와 num[idx+1] 뺀다 => 그 자리에 result값을 넣는다.
-
-문제점 : expression은 안없어지는데.. num은 계속 사라짐... => 
-
-100-200*300-500+20
-100-60000-500+20
--연산을 수행할 때 
-0 1 2
-
--59900-480 => 이거일 때
-
-
-+100+100+100
-200
+1. 연산자 우선순위를 정하기 위한 조합
+    => 3개밖에 없기때문에 직접 패턴을 만드는게 좋음.
+2. expression을 숫자, 연산자 로 나누어서 vector에 저장한다.
+2. 우선 순위를 먼저 계산을 수행하는데, 음수는 음수, 덧셈은 덧셈이 더 나올 수 있기 때문에 1개의 결과값이 나올 때까지 반복
 */
