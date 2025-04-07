bfcdcfad257b492dab3deb5d244dafb7354a47af
suyoung <81684148+songsuyoung@users.noreply.github.com>
2025-04-08 08:50:57 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin>>N;
+
+	vector<int> arr(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin>>arr[i];
+	}
+
+	sort(arr.begin(), arr.end());
+
+	for (int i = 0; i < N; i++)
+	{
+		cout<<arr[i]<<'\n';
+	}
+	
+}
