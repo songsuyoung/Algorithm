5a301c7c34be4eb1204190b7edb1311fb453f052
suyoung <81684148+songsuyoung@users.noreply.github.com>
2025-04-08 10:19:21 +0900

    [Silver II] Title: 좌표 압축, Time: 1736 ms, Memory: 65200 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	vector<int> arr(N);
+	set<int> st;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+		st.insert(arr[i]);
+	}
+
+	map<int, int> sorting;
+
+	int idx=0;
+	for (const auto& order : st)
+	{
+		sorting[order] = idx;
+		idx++;
+	}
+
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cout<<sorting[arr[i]] << ' ';
+	}
+}
+
+//수, 빈도, 순위
+//수 정렬 => 순위 (map=> 정렬)
+//실제 수 보관 vector
