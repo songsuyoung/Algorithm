68c111f7c664b6fae308cd4eb74efc1a6749bfcb
suyoung <81684148+songsuyoung@users.noreply.github.com>
2025-04-26 10:03:26 +0900

    [level 3] Title: 양과 늑대, Time: 4.54 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/README.md
+++ 프로그래머스/3/92343. 양과 늑대/README.md
@@ -1,10 +1,10 @@
 # [level 3] 양과 늑대 - 92343 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343) 
 
 ### 성능 요약
 
-메모리: 3.69 MB, 시간: 4.03 ms
+메모리: 4.23 MB, 시간: 4.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 22일 10:59:25
+2025년 04월 26일 10:03:24
 
 ### 문제 설명
 
--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -1,83 +1,75 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int isVisited[20][20][20];
-
-int answer = 0;
-void dfs(vector<int> graph[], vector<int> info, int cur, int sheep, int woolf)
+bool isVisited[20][20][20];
+int answer;
+void dfs(vector<vector<int>>& tree, vector<int> info, int idx, int sheep, int woolf)
 {
-    //root일 때 최대 개수를 확인해봄 -> 여러번 방문해야하기 때문에 max값을 root에서 확인해주어야함.
-    if(cur==0)
+    if(idx == 0)
     {
+        //루트일 때마다 회수한 수를 확인한다.
         answer = max(answer, sheep);
     }
     
-    for(int i=0; i<graph[cur].size(); i++)
+    for(int i=0; i<tree[idx].size(); i++)
     {
-        int next = graph[cur][i];
-        if(info[next] == 0)
+        int node = tree[idx][i]; //현재 노드의 위치
+        
+        if(info[node] == 1)
         {
-            //다음 위치로 이동했을 때 sheep 개수가 이미 방문 했음 갔다온거임
-            if(isVisited[next][sheep + 1][woolf]) continue;
-            info[next] = -1; //양 회수
-            isVisited[next][sheep+1][woolf] = true;
-            //이전으로 돌아가야하기 때문에 방문처리, 현재 위치 - 양의 개수 - 늑대의 개수로 체크해야한다.
-            dfs(graph, info, next, sheep + 1, woolf);
-            info[next] = 0; //양 회수
-            isVisited[next][sheep+1][woolf] = false;
-        }
-        else if(info[next] == 1)
-        {
-            //늑대임
-            if(sheep > woolf + 1)
+            //늑대
+            if(sheep > woolf + 1 && isVisited[node][sheep][woolf + 1] == false)
             {
-                if(isVisited[next][sheep][woolf+1]) continue;
-                isVisited[next][sheep][woolf+1] = true;
-                info[next] = -1; //늑대 회수
-                //이동 가능함.
-                dfs(graph,info, next, sheep, woolf + 1);
-                info[next] = 1; //늑대 돌려놓음
-                isVisited[next][sheep][woolf+1] = false;
+                //sheep의 수는 항상 woolf보다 커야함. 그럴 때 지나갈 수 있음.
+                isVisited[node][sheep][woolf + 1] = true;
+                info[node] = -1;
+                dfs(tree, info, node, sheep, woolf + 1);
+                info[node] = 1;
+                isVisited[node][sheep][woolf + 1] = false;
             }
+        }
+        else if(info[node] == 0 && isVisited[node][sheep + 1][woolf] == false)
+        {
+            //항상 지나감
+            isVisited[node][sheep + 1][woolf]  = true;
+            info[node] = -1;
+            dfs(tree, info, node, sheep + 1, woolf);
+            info[node] = 0;
+            isVisited[node][sheep + 1][woolf]  = false;
         }
         else
         {
-            if(isVisited[next][sheep][woolf]) continue;
-            //이미 회수하고 다른 늑대나 양을 찾기 위해 지나감
-            isVisited[next][sheep][woolf] = true;
-            dfs(graph, info, next, sheep, woolf);
-            isVisited[next][sheep][woolf] = false;
-            
+            if(isVisited[node][sheep][woolf] == false)
+            {
+                isVisited[node][sheep][woolf]  = true;
+                //이미 회수해서 그냥 지나갈 수 있음.
+                dfs(tree, info, node, sheep, woolf);
+                isVisited[node][sheep][woolf]  = false;
+            }
         }
     }
 }
+
 int solution(vector<int> info, vector<vector<int>> edges) {
-    // 그래프 생성
-    vector<int> graph[20];
+    vector<vector<int>> tree(info.size());
     
-    //info는 부모의 정보, edgs 는 부모-자식간의 관계
     for(int i=0; i<edges.size(); i++)
     {
-        // 양방향 그래프로 성생
-        graph[edges[i][0]].push_back(edges[i][1]);
-        graph[edges[i][1]].push_back(edges[i][0]); 
+        //회수를 위해서 트리를 양방향으로 만든다.
+        tree[edges[i][0]].push_back(edges[i][1]);
+        tree[edges[i][1]].push_back(edges[i][0]);
     }
     
-    //root는 항상 양이 존재한다.
-    info[0] = -1; //첫번째 양은 항상 회수
-    isVisited[0][1][0] = true;
-    dfs(graph, info, 0, 1, 0); //양의 개수, 늑대의 개수
+    //항상 루트부터 시작하고, 루트는 항상 양이기 때문에 항상 회수할 수 있다.
+   isVisited[0][1][0] = true;
+    info[0] = -1;
+    dfs(tree, info, 0, 1, 0); //0에서 시작
     return answer;
 }
 
 /*
-2진 트리 모양 초원 늑대, 양
-루트 노드에서 출발하여 각 노드를 돌아다니며 양을 모으려고 함.
-노드에 방문 할 때마다 양과 늑대가 당신을 따라옴.
-내가 모은 양의 수 <= 늑대 수 
-
-=> 완전탐색 왜냐하면, 모든 노드를 방문 할 때까지 양과 늑대의 수를 알 수 없음. 즉 모두 방문해 봐야한다.
-=> 각 노드의 양의 개수와 늑대의 개수를 적고 양이 max가 되는 노드를 선택한다.
-
+edges 를 연결리스트를 사용해서 트리로 만든다.
+양일 경우에 회수/ 늑대일 경우에 양의 수를 확인 후 회수 한다. => 즉 늑대>=양일 경우 회수하지 않는다.
+이미 회수한 경우는 지나갈 수 있다. (회수할 때마다 -1로 초기화 해준다.)
+가장 큰 수를 찾아준다.
 */
